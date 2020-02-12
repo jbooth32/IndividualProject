766afba27ebdcba3ac1c3a47ac8675a68208ae4f
Jared <boothjar@grinnell.edu>
2020-02-12 11:26:42 -0600

    Finish Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+  @base_title = "Ping Pong App"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    
+
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get add" do
     get static_pages_add_url
     assert_response :success
+    
+    assert_select "title", "Add | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
