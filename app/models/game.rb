a012575efbaca2a8ba9ee8d3ae43a204239c0b8b
Jared <boothjar@grinnell.edu>
2020-04-01 17:48:48 -0500

    HW8: Added a new game form

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,19 @@
+class ScoreValidator < ActiveModel::Validator
+  def validate(game)
+    if game.w_score <= game.l_score
+      game.errors[:base] << "Winner must score more than loser"
+    end
+    if game.l_score < 0
+      game.errors[:base] << "Scores must be positive"
+    end
+  end
+end
+
 class Game < ApplicationRecord
+    validates :winner, presence: true, length: {maximum: 30}
+    validates :loser, presence: true, length: {maximum: 30}
+    VALID_DATE_REGEX = /\A(((0)[0-9])|((1)[0-2]))(\/)([0-2][0-9]|(3)[0-1])(\/)\d{2}/
+    validates :date, format: { with: VALID_DATE_REGEX}
+    validates_with ScoreValidator
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :games
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  get '/add',  to: 'static_pages#add'
+  get '/add',  to: 'games#new'
   get '/randgame', to: 'static_pages#randgame'
 end
